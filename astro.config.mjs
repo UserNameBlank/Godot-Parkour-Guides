bf0f20693d4a2161842267fd2c177633ab202ae9
UserNameBlank <h.kowi2000@gmail.com>
2025-02-08 16:07:01 +0100

    [fix] fix for gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,42 +1,40 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 import { remarkModifiedTime } from "./src/utils/remark-modified-time.mjs";
-import remarkInlineCodeLanguage from "remark-inline-code-language"
+import remarkInlineCodeLanguage from "remark-inline-code-language";
 import partytown from "@astrojs/partytown";
 import pagefind from "astro-pagefind";
 import tailwind from "@astrojs/tailwind";
 
-
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	trailingSlash: "always",
-	prefetch: {
-		prefetchAll: true,
-		defaultStrategy: 'viewport',
-	},
+  site: "https://UserNameBlank.github.io",
+  base: "Godot-Parkour-Guides",
+  trailingSlash: "always",
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "viewport",
+  },
 
-	experimental: {
-		
-	},
+  experimental: {},
 
-	markdown: {
-		remarkPlugins: [remarkModifiedTime, remarkInlineCodeLanguage],
-	},
-	integrations: [
-		mdx(),
-		sitemap(),
-		pagefind(),
-		tailwind(),
+  markdown: {
+    remarkPlugins: [remarkModifiedTime, remarkInlineCodeLanguage],
+  },
+  integrations: [
+    mdx(),
+    sitemap(),
+    pagefind(),
+    tailwind(),
 
-		partytown({
-			config: {
-				forward: ["dataLayer.push"],
-				debug: false,
-			},
-		}),
-	],
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+        debug: false,
+      },
+    }),
+  ],
 });
